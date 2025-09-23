e9c1cf4ddc24fc87255d4e6ea0d79fd0ee05084a
Artem Karlov <ar.karlov@gmail.com>
2025-09-23 22:59:59 +0200

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
 export default tseslint.config([
   globalIgnores(['dist']),
   {
@@ -20,4 +22,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  eslintConfigPrettier,
 ])
